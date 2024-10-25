c5576dcf6d6646f0098aa3754f225ff6e417f0b2
mabindy <mabindy150@gmail.com>
2024-10-25 10:27:34 -0700

    Added full files for PyCraft and added world saving

--- PyCraft/pycraft.py
+++ PyCraft/pycraft.py
@@ -6,8 +6,12 @@ from perlin_noise import PerlinNoise
 from ursina.collider import BoxCollider
 from ursina.texture_importer import load_texture
 import random
+import json
+import os
 
 app = Ursina(borderless=False, title='PyCraft', icon='pycraftlogo.ico')
+
+world_data = []
 
 window.fullscreen = False
 
@@ -25,13 +29,14 @@ class Voxel(Button):
             origin_y=.5,
             texture='pycrafttextures/cobblestone.png',
             color=base_color,
+            isblock = True
         )
         r = min(base_color.r + 0.1, 1.0)
         g = min(base_color.g + 0.1, 1.0)
         b = min(base_color.b + 0.1, 1.0)
         self.highlight_color = color.rgb(r, g, b)
 
-class OreVoxel(Button):
+class IronOreVoxel(Button):
     def __init__(self, position=(0,0,0)):
         base_color = color.hsv(0, 0, random.uniform(.9, 1.0))
         super().__init__(parent=scene,
@@ -40,12 +45,60 @@ class OreVoxel(Button):
             origin_y=.5,
             texture='pycrafttextures/iron_ore.png',
             color=base_color,
+            isblock = True
         )
         r = min(base_color.r + 0.1, 1.0)
         g = min(base_color.g + 0.1, 1.0)
         b = min(base_color.b + 0.1, 1.0)
         self.highlight_color = color.rgb(r, g, b)
 
+class CoalOreVoxel(Button):
+    def __init__(self, position=(0,0,0)):
+        base_color = color.hsv(0, 0, random.uniform(.9, 1.0))
+        super().__init__(parent=scene,
+            position=position,
+            model='cube',
+            origin_y=.5,
+            texture='pycrafttextures/coal_ore.png',
+            color=base_color,
+            isblock = True
+        )
+        r = min(base_color.r + 0.1, 1.0)
+        g = min(base_color.g + 0.1, 1.0)
+        b = min(base_color.b + 0.1, 1.0)
+        self.highlight_color = color.rgb(r, g, b)
+
+class OakPlanksVoxel(Button):
+    def __init__(self, position=(0,0,0)):
+        base_color = color.hsv(0, 0, .9)
+        super().__init__(parent=scene,
+            position=position,
+            model='cube',
+            origin_y=.5,
+            texture='pycrafttextures/oak_planks.png',
+            color=base_color,
+            isblock = True
+        )
+        r = min(base_color.r + 0.1, 1.0)
+        g = min(base_color.g + 0.1, 1.0)
+        b = min(base_color.b + 0.1, 1.0)
+        self.highlight_color = color.rgb(r, g, b)
+
+class GlassVoxel(Button):
+    def __init__(self, position=(0,0,0)):
+        base_color = color.hsv(0, 0, .9)
+        super().__init__(parent=scene,
+            position=position,
+            model='cube',
+            origin_y=.5,
+            texture='pycrafttextures/glass.png',
+            color=base_color,
+            isblock = True
+        )
+        r = min(base_color.r + 0.1, 1.0)
+        g = min(base_color.g + 0.1, 1.0)
+        b = min(base_color.b + 0.1, 1.0)
+        self.highlight_color = color.rgb(r, g, b)
 
 class GroundVoxel(Button):
     def __init__(self, position=(0,0,0)):
@@ -57,6 +110,7 @@ class GroundVoxel(Button):
             texture='pycrafttextures/grass_top.png',
             color=base_color,
             highlight_color=color.cyan,
+            isblock = True
         )
         r = min(base_color.r + 0.1, 1.0)
         g = min(base_color.g + 0.1, 1.0)
@@ -73,12 +127,33 @@ class BrownVoxel(Button):
             texture='pycrafttextures/default_dirt.png',
             color=base_color,
             highlight_color=color.cyan,
+            collider='box',
+            isblock = True
+        )
+        r = min(base_color.r + 0.1, 1.0)
+        g = min(base_color.g + 0.1, 1.0)
+        b = min(base_color.b + 0.1, 1.0)
+        self.highlight_color = color.rgb(r, g, b)
+
+
+class DoorVoxel(Button):
+    def __init__(self, position=(0,0,0)):
+        base_color = color.hsv(30, 0.5, 0.7)
+        super().__init__(parent=scene,
+            position=position,
+            model='cube',
+            scale = (1,2,0.25),
+            origin_y=.5,
+            texture='pycrafttextures/oak_door.png',
+            color=base_color,
+            highlight_color=color.cyan,
             collider='box'
         )
         r = min(base_color.r + 0.1, 1.0)
         g = min(base_color.g + 0.1, 1.0)
         b = min(base_color.b + 0.1, 1.0)
         self.highlight_color = color.rgb(r, g, b)
+
 class Bedrock(Button):
     def __init__(self, position=(0,0,0)):
         base_color = color.hsv(0, 0, 1)
@@ -89,6 +164,7 @@ class Bedrock(Button):
             texture='pycrafttextures/bedrocktexture.png',
             color=base_color,
             highlight_color=color.cyan,
+            isblock = True
         )
         r = min(base_color.r + 0.1, 1.0)
         g = min(base_color.g + 0.1, 1.0)
@@ -99,8 +175,8 @@ class Bedrock(Button):
 
 
 noise = PerlinNoise (octaves=3, seed=random.randint(1,1000000))
-min_y = -5
-worlddimensions = 15 #World dimensions are twice this number 
+min_y = -10
+worlddimensions = 10 #World dimensions are twice this number 
 for z in range(-worlddimensions,worlddimensions):
     for x in range(-worlddimensions,worlddimensions):
         surface_y = noise([x * .02,z * .02])
@@ -109,16 +185,25 @@ for z in range(-worlddimensions,worlddimensions):
             position = (x, y, z)
             if y == surface_y:
                 voxel = GroundVoxel(position=position)
+                block_type = 'GroundVoxel'
             elif y == min_y:
                 voxel = Bedrock(position=position)
+                block_type = 'Bedrock'
             elif y > surface_y - 3:
                 voxel = BrownVoxel(position=position)
+                block_type = 'BrownVoxel'
             else:
-                oregenerator = random.randint(0,10)
-                if oregenerator == 5:
-                    voxel = OreVoxel(position=position)
+                oregenerator = random.randint(0,20)
+                if oregenerator == 5 and y < surface_y - 10:
+                    voxel = IronOreVoxel(position=position)
+                    block_type = 'IronOreVoxel'
+                elif oregenerator == 15:
+                    voxel = CoalOreVoxel(position=position)
+                    block_type = 'CoalOreVoxel'
                 else:
                     voxel = Voxel(position=position)
+                    block_type = 'Voxel'
+            world_data.append({'position': [x, y, z], 'block_type': block_type})
 wall_thickness = 1
 wall_height = 200
 voxel_size = 1
@@ -140,6 +225,7 @@ north_wall = Entity(
     collider = 'box',
     visible = False,
     destroyable = False,
+    wall = True
 )
 south_wall = Entity(
     model = 'cube',
@@ -152,6 +238,7 @@ south_wall = Entity(
     collider = 'box',
     visible = False,
     destroyable = False,
+    wall = True
 )
 east_wall = Entity(
     model = 'cube',
@@ -164,18 +251,20 @@ east_wall = Entity(
     collider = 'box',
     visible = False,
     destroyable = False,
+    wall = True
 )
 west_wall = Entity(
     model = 'cube',
     scale = (wall_thickness, wall_height, terrain_width_xz),
     position=(
-        terrain_min_xz + wall_thickness / 2,
+        terrain_min_xz + wall_thickness / 2 - 1,
         wall_height / 2 + min_y,
         (terrain_min_xz + terrain_max_xz) / 2
     ),
     collider = 'box',
     visible = False,
     destroyable = False,
+    wall = True
 )
 
 hotbar = Button( 
@@ -207,6 +296,48 @@ def update_hand_position():
         hand.position = (0.5, -0.5, 2)
     else:
         hand.position = (0.5, -0.5, 1)
+
+def clear_world():
+    for entity in scene.entities[:]:
+        if isinstance(entity, Button) and entity != player and hasattr(entity, 'isblock') and not hasattr(entity, 'wall'):
+            destroy(entity)
+
+def rebuild_world_from_data():
+    for block in world_data:
+        position = block['position']
+        block_type = block['block_type']
+        x, y, z = position
+        position = Vec3(x,y,z)
+
+        if block_type == 'Voxel':
+            Voxel(position=position)
+        elif block_type == 'GroundVoxel':
+            GroundVoxel(position=position)
+        elif block_type == 'BrownVoxel':
+            BrownVoxel(position=position)
+        elif block_type == 'IronOreVoxel':
+            IronOreVoxel(position=position)
+        elif block_type == 'CoalOreVoxel':
+            CoalOreVoxel(position=position)
+        elif block_type == 'Bedrock':
+            Bedrock(position=position)
+        elif block_type == 'OakPlanksVoxel':
+            OakPlanksVoxel(position=position)
+        elif block_type == 'GlassVoxel':
+            GlassVoxel(position=position)
+        
+
+def save_world(filename=r'C:\Users\mberr954\Documents\world_save.json'):
+    with open(filename, 'w') as f:
+        json.dump(world_data, f)
+
+def load_world(filename=r'C:\Users\mberr954\Documents\world_save.json'):
+    global world_data
+    with open(filename, 'r') as f:
+        world_data = json.load(f)
+    clear_world()
+    rebuild_world_from_data()
+
 
 
 settings_opened = False
@@ -295,9 +426,11 @@ def destroy_pause_menu():
     destroy(quit_button)
     destroy(settings_button)
     destroy(pause_label)
+    destroy(save_button)
+    destroy(load_button)
 
 def build_pause_menu():
-    global resume_button, quit_button, pause_label, settings_button
+    global resume_button, quit_button, pause_label, settings_button, save_button, load_button
     resume_button = Button(
         parent=pause_menu,
         text='Resume',
@@ -307,12 +440,30 @@ def build_pause_menu():
         on_click = lambda: toggle_mouse_lock()
     )
 
+    save_button = Button(
+        parent=pause_menu,
+        text='Save World',
+        color=color.gray,
+        scale=(0.15, 0.02), 
+        position=(0, -0.1),  
+        on_click = lambda: save_world()
+    )
+
+    load_button = Button(
+        parent=pause_menu,
+        text='Load World',
+        color=color.gray,
+        scale=(0.15, 0.02), 
+        position=(0, -0.2),  
+        on_click = lambda: load_world()
+    )
+
     quit_button = Button(
         parent=pause_menu,
         text='Quit',
         color=color.gray,
         scale=(0.15, 0.02), 
-        position=(0, -0.1),  
+        position=(0, -0.15),  
         on_click = application.quit
     )
     settings_button = Button(
@@ -340,28 +491,11 @@ defrot = (0,0,0)
 selectedvoxel = Voxel
 selected = ''
 def input(key):
-    global selectedvoxel
-    global selector
-    global regvoxel
-    global brovoxel
-    global hand
-    global defrot
-    global selected
+    global selectedvoxel, selector, regvoxel, brovoxel, hand, defrot, selected, world_data, blockcopied
     if key == 'escape':
             toggle_mouse_lock()
         
     if mouse.locked:
-        if key == 'right mouse down':
-            hit_info = raycast(camera.world_position, camera.forward, distance=5, ignore=(player,))
-            if hit_info.hit and selectedvoxel:
-                selectedvoxel(position=hit_info.entity.position + hit_info.normal)
-                
-                hand.rotation = defrot
-
-                hand.animate_rotation((110, -30, 0), duration=0.2, curve=curve.in_out_quad)
-                        
-                invoke(hand.animate_rotation, defrot, duration=0.2, curve=curve.in_out_quad, delay=0.2)
-                
         if (key == 'left mouse down' and mouse.hovered_entity and mouse.hovered_entity != player) or (key == 'left mouse down' and selected == 'ak'):
             if hasattr(mouse.hovered_entity, 'destroyable') and not mouse.hovered_entity.destroyable:
 
@@ -372,8 +506,14 @@ def input(key):
                         
                     invoke(hand.animate_rotation, defrot, duration=0.1, curve=curve.in_out_expo, delay=0.1)
                 else:
+                    position = mouse.hovered_entity.position
                     destroy(mouse.hovered_entity)
-                
+
+                    for block in world_data:
+                        if block['position'] == [position.x, position.y, position.z]:
+                            world_data.remove(block)
+                            break
+
                     hand.rotation = defrot
 
                     hand.animate_rotation((110, -30, 0), duration=0.2, curve=curve.in_out_quad)
@@ -381,7 +521,9 @@ def input(key):
                     invoke(hand.animate_rotation, defrot, duration=0.2, curve=curve.in_out_quad, delay=0.2)
         
         if key == 'middle mouse down' and mouse.hovered_entity:
+            global blockcopied
             voxcolor = mouse.hovered_entity.color
+            blockcopied = mouse.hovered_entity.__class__.__name__
             blocktexture =mouse.hovered_entity.texture
             if hasattr(mouse.hovered_entity, 'destroyable'):
                 class CopiedVoxel(Button):
@@ -422,6 +564,21 @@ def input(key):
             destroy(selector)
             destroy(hand)
             hand = Entity(model='cube',texture=blocktexture, color=color.rgb(voxcolor.r, voxcolor.g, voxcolor.b), scale=(0.5,0.5,0.5), rotation=(0,0,0), position=(0,0,0), parent=camera)
+        if key == 'right mouse down':
+            hit_info = raycast(camera.world_position, camera.forward, distance=5, ignore=(player,))
+            if hit_info.hit and selectedvoxel and not hasattr(hit_info.entity, 'wall'):
+                new_position = hit_info.entity.position + hit_info.normal
+                selectedvoxel(position=new_position)
+                block_type = selectedvoxel.__name__
+                if block_type == 'CopiedVoxel':
+                    block_type = blockcopied
+                world_data.append({'position': [new_position.x, new_position.y, new_position.z], 'block_type': block_type})
+                hand.rotation = defrot
+
+                hand.animate_rotation((110, -30, 0), duration=0.2, curve=curve.in_out_quad)
+                        
+                invoke(hand.animate_rotation, defrot, duration=0.2, curve=curve.in_out_quad, delay=0.2)
+                
         if key == '1':
             selectedvoxel = Voxel
             selected = 'basic'
@@ -460,6 +617,21 @@ def input(key):
             selected = 'ak'
             hand = Entity(model="ak.obj", scale=(0.5,0.5,0.5), position=(0,0,0), rotation=(180,0,180), parent=camera)
             defrot = hand.rotation
+        if key == '5':
+            selectedvoxel = OakPlanksVoxel
+            selected = 'oakplanks'
+            destroy(hand)
+            hand = Entity(model='cube',texture='pycrafttextures/oak_planks.png', color=color.hsv(0, 0, .9), scale=(0.5,0.5,0.5), rotation=(0,0,0), position=(0,0,0), parent=camera)
+            defrot = hand.rotation
+            destroy(selector)
+        if key == '6':
+            selectedvoxel = GlassVoxel
+            selected = 'glass'
+            destroy(hand)
+            hand = Entity(model='cube',texture='pycrafttextures/glass.png', color=color.hsv(0, 0, .9), scale=(0.5,0.5,0.5), rotation=(0,0,0), position=(0,0,0), parent=camera)
+            defrot = hand.rotation
+            destroy(selector)
+
 issprinting = False
 iscrouching = False
 def update():
@@ -480,6 +652,6 @@ def update():
 player = FirstPersonController()
 player.height = 1.8
 player.camera_pivot.y = 1.8
-Sky()
 
+Sky()
 app.run()
