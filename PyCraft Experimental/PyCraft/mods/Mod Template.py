d18e63108ceb400d2990fd1acf146cd643778a3b
mabindy <mabindy150@gmail.com>
2024-11-07 10:01:11 -0700

    Added Mod Template

---
+++ PyCraft Experimental/PyCraft/mods/Mod Template.py
@@ -0,0 +1,12 @@
+from ursina import *
+
+# Initializes custom blocks and features in mod
+def initialize(game_api):
+    # Your code here...
+    print('(Your Mod Name) Mod Initialized!')
+
+
+# Deinitializes everything that was added in initialize()
+def deinitialize(game_api):
+    #Your code here...
+    print('(Your Mod Name) Mod Deinitialized')
